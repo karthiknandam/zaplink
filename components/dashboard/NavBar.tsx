28a53729ff654ac78aed41e11ba50f21b55dc656
Karthik Nandam <carthicknandam@gmail.com>
2025-11-30 01:21:36 +0530

    style: broken navbar width fix

--- components/dashboard/NavBar.tsx
+++ components/dashboard/NavBar.tsx
@@ -6,7 +6,7 @@ import { Create } from "./Create";
 export const NavBar = () => {
   const isMobile = useIsMobile();
   return (
-    <nav className="h-20 w-full border-b border-b-sidebar-border flex items-center justify-between px-5 fixed top-0 z-50 bg-background">
+    <nav className="h-20 max-md:w-full md:w-[calc(100vw-18rem)] border-b border-b-sidebar-border flex items-center justify-between px-5 fixed top-0 z-50 bg-background">
       <header>
         <h1 className="font-bold text-xl text-secondary-foreground">
           {isMobile && <SidebarTrigger className="mr-2 inline-flex" />}
